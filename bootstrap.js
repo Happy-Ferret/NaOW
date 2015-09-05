6b3a28cd59838092b0ac4f4026227725c9e47158
Noitidart <noitidart@gmail.com>
2015-09-05 14:47:27 -0700

    1.0-night.initial
    Setup my typical jsctypes and localized format

---
+++ bootstrap.js
@@ -0,0 +1,167 @@
+// Imports
+const {classes: Cc, interfaces: Ci, manager: Cm, results: Cr, utils: Cu, Constructor: CC} = Components;
+// Cm.QueryInterface(Ci.nsIComponentRegistrar);
+
+Cu.import('resource://gre/modules/devtools/Console.jsm');
+Cu.import('resource://gre/modules/osfile.jsm');
+Cu.import('resource://gre/modules/Services.jsm');
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
+
+// Globals
+const core = {
+	addon: {
+		name: 'NaOW',
+		id: 'NaOW@jetpack',
+		path: {
+			name: 'naow',
+			content: 'chrome://naow/content/',
+			images: 'chrome://naow/content/resources/images/',
+			locale: 'chrome://naow/locale/',
+			modules: 'chrome://naow/content/resources/modules/',
+			resources: 'chrome://naow/content/resources/',
+			scripts: 'chrome://naow/content/resources/scripts/',
+			styles: 'chrome://naow/content/resources/styles/',
+			workers: 'chrome://naow/content/resources/modules/workers/'
+		},
+		cache_key: Math.random() // set to version on release
+	},
+	os: {
+		name: OS.Constants.Sys.Name.toLowerCase(),
+		toolkit: Services.appinfo.widgetToolkit.toLowerCase(),
+		xpcomabi: Services.appinfo.XPCOMABI
+	},
+	firefox: {
+		pid: Services.appinfo.processID,
+		version: Services.appinfo.version
+	}
+};
+
+const JETPACK_DIR_BASENAME = 'jetpack';
+const myPrefBranch = 'extensions.' + core.addon.id + '.';
+
+// Lazy Imports
+const myServices = {};
+XPCOMUtils.defineLazyGetter(myServices, 'hph', function () { return Cc['@mozilla.org/network/protocol;1?name=http'].getService(Ci.nsIHttpProtocolHandler); });
+XPCOMUtils.defineLazyGetter(myServices, 'sb', function () { return Services.strings.createBundle(core.addon.path.locale + 'bootstrap.properties?' + core.addon.cache_key); /* Randomize URI to work around bug 719376 */ });
+
+function extendCore() {
+	// adds some properties i use to core based on the current operating system, it needs a switch, thats why i couldnt put it into the core obj at top
+	switch (core.os.name) {
+		case 'winnt':
+		case 'winmo':
+		case 'wince':
+			core.os.version = parseFloat(Services.sysinfo.getProperty('version'));
+			// http://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
+			if (core.os.version == 6.0) {
+				core.os.version_name = 'vista';
+			}
+			if (core.os.version >= 6.1) {
+				core.os.version_name = '7+';
+			}
+			if (core.os.version == 5.1 || core.os.version == 5.2) { // 5.2 is 64bit xp
+				core.os.version_name = 'xp';
+			}
+			break;
+			
+		case 'darwin':
+			var userAgent = myServices.hph.userAgent;
+
+			var version_osx = userAgent.match(/Mac OS X 10\.([\d\.]+)/);
+
+			
+			if (!version_osx) {
+				throw new Error('Could not identify Mac OS X version.');
+			} else {
+				var version_osx_str = version_osx[1];
+				var ints_split = version_osx[1].split('.');
+				if (ints_split.length == 1) {
+					core.os.version = parseInt(ints_split[0]);
+				} else if (ints_split.length >= 2) {
+					core.os.version = ints_split[0] + '.' + ints_split[1];
+					if (ints_split.length > 2) {
+						core.os.version += ints_split.slice(2).join('');
+					}
+					core.os.version = parseFloat(core.os.version);
+				}
+				// this makes it so that 10.10.0 becomes 10.100
+				// 10.10.1 => 10.101
+				// so can compare numerically, as 10.100 is less then 10.101
+				
+				//core.os.version = 6.9; // note: debug: temporarily forcing mac to be 10.6 so we can test kqueue
+			}
+			break;
+		default:
+			// nothing special
+	}
+	
+
+}
+
+// START - Addon Functionalities					
+
+// END - Addon Functionalities
+
+function install() {}
+
+function uninstall() {}
+
+function startup(aData, aReason) {
+	// core.addon.aData = aData;
+	extendCore();
+}
+
+function shutdown(aData, aReason) {
+	if (aReason == APP_SHUTDOWN) { return }
+
+}
+
+// start - common helper functions
+function aReasonMax(aReason) {
+	var deepestReason = aReason;
+	while (deepestReason.hasOwnProperty('aReason') || deepestReason.hasOwnProperty()) {
+		if (deepestReason.hasOwnProperty('aReason')) {
+			deepestReason = deepestReason.aReason;
+		} else if (deepestReason.hasOwnProperty('aCaught')) {
+			deepestReason = deepestReason.aCaught;
+		}
+	}
+	return deepestReason;
+}
+function Deferred() {
+	// update 062115 for typeof
+	if (typeof(Promise) != 'undefined' && Promise.defer) {
+		//need import of Promise.jsm for example: Cu.import('resource:/gree/modules/Promise.jsm');
+		return Promise.defer();
+	} else if (typeof(PromiseUtils) != 'undefined'  && PromiseUtils.defer) {
+		//need import of PromiseUtils.jsm for example: Cu.import('resource:/gree/modules/PromiseUtils.jsm');
+		return PromiseUtils.defer();
+	} else {
+		/* A method to resolve the associated Promise with the value passed.
+		 * If the promise is already settled it does nothing.
+		 *
+		 * @param {anything} value : This value is used to resolve the promise
+		 * If the value is a Promise then the associated promise assumes the state
+		 * of Promise passed as value.
+		 */
+		this.resolve = null;
+
+		/* A method to reject the assocaited Promise with the value passed.
+		 * If the promise is already settled it does nothing.
+		 *
+		 * @param {anything} reason: The reason for the rejection of the Promise.
+		 * Generally its an Error object. If however a Promise is passed, then the Promise
+		 * itself will be the reason for rejection no matter the state of the Promise.
+		 */
+		this.reject = null;
+
+		/* A newly created Pomise object.
+		 * Initially in pending state.
+		 */
+		this.promise = new Promise(function(resolve, reject) {
+			this.resolve = resolve;
+			this.reject = reject;
+		}.bind(this));
+		Object.freeze(this);
+	}
+}
+// end - common helper functions
---
+++ modules/workers/MainWorker.js
@@ -0,0 +1,121 @@
+'use strict';
+
+// Imports
+importScripts('resource://gre/modules/osfile.jsm');
+importScripts('resource://gre/modules/workers/require.js');
+
+// Globals
+var core = {
+	addon: {
+		path: {
+			content: 'chrome://naow/content/',
+		}
+	},
+	os: {
+		name: OS.Constants.Sys.Name.toLowerCase()
+	}
+};
+
+var OSStuff = {}; // global vars populated by init, based on OS
+
+// Imports that use stuff defined in chrome
+// I don't import ostypes_*.jsm yet as I want to init core first, as they use core stuff like core.os.isWinXP etc
+// imported scripts have access to global vars on MainWorker.js
+importScripts(core.addon.path.content + 'modules/cutils.jsm');
+importScripts(core.addon.path.content + 'modules/ctypes_math.jsm');
+
+// Setup PromiseWorker
+var PromiseWorker = require('resource://gre/modules/workers/PromiseWorker.js');
+
+var worker = new PromiseWorker.AbstractWorker();
+worker.dispatch = function(method, args = []) {
+	return self[method](...args);
+},
+worker.postMessage = function(...args) {
+	self.postMessage(...args);
+};
+worker.close = function() {
+	self.close();
+};
+worker.log = function(...args) {
+	dump('Worker: ' + args.join(' ') + '\n');
+};
+self.addEventListener('message', msg => worker.handleMessage(msg));
+
+////// end of imports and definitions
+
+function init(objCore) {
+	//console.log('in worker init');
+	
+	// merge objCore into core
+	// core and objCore is object with main keys, the sub props
+	
+	core = objCore;
+
+	// if (core.os.toolkit == 'gtk2') {
+		// core.os.name = 'gtk';
+	// }
+	
+	// I import ostypes_*.jsm in init as they may use things like core.os.isWinXp etc
+	switch (core.os.toolkit.indexOf('gtk') == 0 ? 'gtk' : core.os.name) {
+		case 'winnt':
+		case 'winmo':
+		case 'wince':
+			importScripts(core.addon.path.content + 'modules/ostypes_win.jsm');
+			break
+		case 'gtk':
+			importScripts(core.addon.path.content + 'modules/ostypes_x11.jsm');
+			break;
+		case 'darwin':
+			importScripts(core.addon.path.content + 'modules/ostypes_mac.jsm');
+			break;
+		default:
+			throw new Error({
+				name: 'addon-error',
+				message: 'Operating system, "' + OS.Constants.Sys.Name + '" is not supported'
+			});
+	}
+	
+	// OS Specific Init
+	switch (core.os.toolkit.indexOf('gtk') == 0 ? 'gtk' : core.os.name) {
+		default:
+			// do nothing special
+	}
+	
+	return true;
+}
+
+// Start - Addon Functionality
+function showOpenWithDialog(aOptions) {
+	
+	switch (core.os.toolkit.indexOf('gtk') == 0 ? 'gtk' : core.os.name) {
+		case 'winnt':
+			
+				
+				
+			break;
+		case 'gtk':
+			
+				
+			
+			break;
+		case 'darwin':
+			
+				try {
+					var myNSStrings = new ostypes.HELPER.nsstringColl();
+					
+
+				} finally {					
+					if (myNSStrings) {
+						myNSStrings.releaseAll()
+					}
+				}
+			
+			break;
+		default:
+			console.error('os not supported');
+	}
+	
+}
+
+// End - Addon Functionality
